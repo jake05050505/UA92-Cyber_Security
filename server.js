93d971bf3a56f06dec88d63e4755ad4a7b49faa4
jake05050505 <jakehancox12@gmail.com>
2025-07-05 13:20:24 +0100

    remove server.js post logic

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
 // This file is built using Tim Edwards' notion documents found at:
 // (https://dull-ceres-c2a.notion.site/Cyber-Security-Risk-Extra-Material-1aa408bc87ac80c5a62be0bc3ee23023)
 
-// Adds Express to our project
 const express = require('express');
 const path = require('path');
 
-// Creates the Express application
 const app = express();
 const PORT = 3000;
 
-// Used for database connection
+// database credentials
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-    host: 'localhost',    // Since your database runs locally, use 'localhost'
-    user: 'root',         // Your MySQL username
-    password: 'password', // Your MySQL password
-    database: 'accounts'  // The database you created for this project
+    host: 'localhost',
+    user: 'root',
+    password: 'password', // in a production environment (not localhost) this should be a very strong password to prevent brute force attacks.
+    database: 'accounts'
 });
 
-app.set('view engine', 'ejs'); // Sets the view engine to EJS
-app.set('views', path.join(__dirname, 'views')); // Make sure views are served from the "views" folder that we created
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-// -- SIGN UP ROUTES
+// Routes
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password){
+        res.status(400).send("Please fill all fields")
+    }
+
+});
+
 app.get('/', (req, res) => {
     res.render('login');
 });
@@ -40,22 +51,10 @@ app.post('/login', (req, res) => {
     
 });
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
-app.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-
-    
-
-});
-
 app.get('/dashboard', (req, res) => {
     res.render('dashboard');
 });
 
-//Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
