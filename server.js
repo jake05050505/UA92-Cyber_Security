00d5845d62f5ed2f143f46f8f1fcf16fdd746fd3
jake05050505 <jakehancox12@gmail.com>
2025-07-09 17:25:56 +0100

    add logic for login, refactor

--- server.js
+++ server.js
@@ -1,72 +1,80 @@
 // This file is built using Tim Edwards' notion documents found at:
 // (https://dull-ceres-c2a.notion.site/Cyber-Security-Risk-Extra-Material-1aa408bc87ac80c5a62be0bc3ee23023)
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 // database credentials
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password', // in a production environment (not localhost) this should be a strong password to prevent brute force attacks.
-    database: 'accounts'
+    host: "localhost",
+    user: "root",
+    password: "password", // in a production environment (not localhost) this should be a strong password to prevent brute force attacks.
+    database: "accounts"
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
 // Routes
-app.get('/index', (req, res) => {
-    res.render('index')
+app.get("/index", (req, res) => {
+    res.render("index")
 })
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
+app.get("/signup", (req, res) => {
+    res.render("signup");
 });
 
-app.post('/signup', (req, res) => {
+app.post("/signup", (req, res) => {
     const { email, username, password } = req.body;
 
     if(!email || !username || !password){
-        return res.status(400).render('signup', { error: "Please fill all fields" });
-    };
+        return res.status(400).render("signup", { error: "Please fill all fields" });
+    }
     if(email.length > 64 || username.length > 32 || password.length > 32){
-        return res.status(400).render('signup', { error: "Email/Username/Password too long, please try again"}); // Should only show up if the user edits the html to remove the maxlength attribute
-    };
+        return res.status(400).render("signup", { error: "Email/Username/Password too long, please try again"}); // Should only show up if the user edits the html to remove the maxlength attribute
+    }
+    if (!email.includes('@') || !email.includes('.')){
+        return res.status(400).render("signup")
+    }
 
     console.log(`${email},${username},${password}`);
     const query = "INSERT INTO `users` (`email`, `username`, `password`) VALUES ('" + email + "', '" + username + "', '" + password + "')";
     db.query(query);
 
-    res.redirect('dashboard');
+    res.redirect("dashboard");
 });
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.render("login");
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     const { username, password } = req.body;
 
     if(!username || !password){
-        res.status(400).send("Please fill all fields")
+        res.status(400).send("Please fill all fields");
+    }
+
+    found_password = db.query("Select password from `users` where username = " + username);
+    if (password == found_password){
+        res.redirect("dashboard");
     }
 });
 
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
 });
 
 app.listen(PORT, () => {
@@ -75,8 +83,8 @@ app.listen(PORT, () => {
 
 db.connect((err) => {
     if (err) {
-        console.error('Database connection failed: ' + err.stack);
+        console.error("Database connection failed: " + err.stack);
         return;
-    };
-    console.log('Connected to the MySQL database.');
+    }
+    console.log("Connected to the MySQL database.");
 });
